bb2a54d4e27a8b82334886c5bb6e122ca6105275
Craig Swank <craigswank@gmail.com>
2014-08-21 08:09:12 -0600

    initial commit

---
+++ directive.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var LoginCtrl = function ($scope, $modalInstance, message) {
+    $scope.message = message;
+    $scope.user = {
+        'name': '',
+        'password': ''
+    };
+    $scope.ok = function (event) {
+        $modalInstance.close($scope.user);
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}
+
+angular.module('cswank.navbar', [])
+    .directive("navbar", ['$rootScope', 'auth', '$localStorage', '$modal', '$dockets', function($rootScope, auth, $localStorage, $modal, $dockets) {
+        return {
+            restrict: "E",
+            replace: true,
+            transclude: true,
+            template: '<div class="row-fluid">  <div class="navbar navbar-inverse navbar-fixed-top">    <div class="navbar-inner">      <div class="container">        <a href="/#/"><img height="62" src="/img/gavel.png"/></a>        <ul class="nav pull-right navbar-nav">          <li ng-show="loggedIn">            <a class="brand" ng-click="logout()" href="">log out {{$storage.username}}</a>          </li>          <li ng-hide="loggedIn">            <a class="brand" ng-click="login()" href="">log in</a>          </li>        </ul>      </div>    </div>  </div></div>',
+            controller: function($scope, $timeout, $modal) {
+                $scope.$storage = $localStorage.$default({
+                    username: ""
+                });
+                $scope.username = "";
+                $('[data-hover="dropdown"]').dropdownHover();
+                $scope.login = function(errorMessage) {
+                    var dlg = $modal.open({
+                        template: '<div>  <div class="modal-header">    <a class="close" data-dismiss="modal">×</a>    <h3>Login</h3>    <a class="error">{{message}}</a>  </div>  <div class="modal-body">    <input placeholder="user name" ng-model="user.name" autocapitalize="off" autocorrect="off" auto-focus/><br/>    <input placeholder="password" type="password" ng-model="user.password" ng-enter="ok()"/>  </div>  <div class="modal-footer">    <button type="button" class="btn btn-default" ng-click="cancel()">Cancel</button>    <button type="button" class="btn btn-primary" ng-enter="ok()" ng-click="ok()">Ok</button>  </div></div>',
+                        controller: LoginCtrl,
+                        resolve: {
+                            message: function () {
+                                return errorMessage;
+                            }
+                        }
+                    });
+                    dlg.result.then(function(user) {
+                        $scope.username = user.name;
+                        $scope.$storage.username = user.name;
+                        $scope.password = user.password;
+                        auth.login($scope.username, $scope.password, function(){
+                            $scope.loggedIn = true;
+                            $rootScope.loggedIn = true;
+                        }, function(){
+                            $scope.login("username or password not correct, please try again");
+                        });
+                    });
+                }
+                
+                $scope.logout = function() {
+                    auth.logout(function() {
+                        $scope.loggedIn = false;
+                    });
+                }
+                function ping() {
+                    $dockets.ping(function(data) {
+                        $scope.loggedIn = true;
+                        $rootScope.loggedIn = true;
+                    }, function() {
+                        $scope.loggedIn = false;
+                        $rootScope.loggedIn = false;
+                        $scope.errMsg = "login failed"
+                    });
+                }
+                $scope.loggedIn = false;
+                $rootScope.loggedIn = false;
+                ping();
+            }
+        }
+    }])
+  });
